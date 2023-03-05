3dd18f42553668eccb78f8d40d7f8506154ac1c1
79tanka <72666478+79tanka@users.noreply.github.com>
2023-03-05 19:23:02 +0200

    changed package

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./icon-github.svg";
+import SearchForm from "./components/SearchForm";
+import SearchResult from "./components/SearchResult";
+import "./App.css";
 
+export interface IResult {
+  html_url: string;
+  name: string;
+  id: number;
+}
 function App() {
+  const [name, setName] = useState("");
+  const [result, setResult] = useState<Array<IResult>>([]);
+
+  const handleChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+  const checkEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") handleSearch();
+  };
+  async function handleSearch() {
+    const response = await fetch(
+      `https://api.github.com/search/repositories?q=${name}+in:name `
+    );
+    if (response.ok) {
+      setResult((await response.json()).items);
+    } else {
+      setResult([]);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
+      <main className="App-main">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Find repository by name on GitHub</p>
+        <SearchForm
+          name={name}
+          handleChangeName={handleChangeName}
+          checkEnter={checkEnter}
+          handleSearch={handleSearch}
+        />
+        <SearchResult result={result} />
+        {!result.length && <span>no result</span>}
+      </main>
     </div>
   );
 }
